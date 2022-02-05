a14ab03e100eafc07141c532d564cff3a3a1002d
H. Vetinari <h.vetinari@gmx.com>
2022-02-05 22:07:05 +1100

    add bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,18 @@
+@echo on
+
+cd libraries/liblmdb/
+
+:: for details on why these values are set, check out the upstream Makefile
+:: https://github.com/LMDB/lmdb/blob/mdb.master/libraries/liblmdb/Makefile
+
+set "DESTDIR=%LIBRARY_PREFIX%"
+set "SOEXT=.dll"
+set "CC=%CC%"
+
+:: only build shared lib, don't build programs & docs
+set "ILIBS=liblmdb.dll"
+set "IPROGS="
+set "IDOCS="
+
+make
+make install
